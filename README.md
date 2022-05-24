c6c9cee30c0f06508bb0d68d8125e95ad0cf372f
polo <lg550055@hotmail.com>
2022-05-24 15:23:53 -0500

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# snack-tracker
-Small Django project with models
+# Lab - Class 27
+
+## Project: Snack Tracker
+Simple Django site with models
+
+### Author: Polo Gonzalez
+
+### Setup
+
+#### Requirements: Django
+
+#### Run: python manage.py runserver
+
+#### Tests: python manage.py test
+
+---
---
+++ snacks/tests.py
@@ -0,0 +1,31 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+# NEEDS update
+class SnacksTests(SimpleTestCase):
+  def test_home_status(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+  
+  def test_home_template(self):
+    url = reverse('home')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, 'home.html')
+    self.assertTemplateUsed(response, 'base.html')
+  
+  def test_about_status(self):
+    url = reverse('about')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+  
+  def test_about_template(self):
+    url = reverse('about')
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, 'about.html')
+    self.assertTemplateUsed(response, 'base.html')
+  
+  def test_not_fount(self):
+    url = 'unknown/'
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 404)
