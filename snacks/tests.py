3c36091250f2bdbd6d5206b115ac4f0fe7516a6b
polo <lg550055@hotmail.com>
2022-05-25 12:03:47 -0500

    Adds setup to tests, which allows all tests to pass

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,8 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
+from .models import Snack
 
 class SnacksTests(TestCase):
+  def setUp(self):
+        purchaser = get_user_model().objects.create(username="tester",password="tester")
+        Snack.objects.create(name="rake", purchaser=purchaser)
+
   def test_home_status(self):
     url = reverse('snack_list')
     response = self.client.get(url)
@@ -19,8 +24,8 @@ class SnacksTests(TestCase):
     response = self.client.get(url)
     self.assertEqual(response.status_code, 200)
   
-  # def test_snack_detail_template(self):
-  #   url = reverse('snack_detail',args=(1,))
-  #   response = self.client.get(url)
-  #   self.assertTemplateUsed(response, 'snack_detail.html')
-  #   self.assertTemplateUsed(response, 'base.html')
+  def test_snack_detail_template(self):
+    url = reverse('snack_detail',args=(1,))
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, 'snack_detail.html')
+    self.assertTemplateUsed(response, 'base.html')
